34e3ef74dee3a6764754a76aa90d5c22f7acec0f
swayam-ekacare <swayam.nayak@ekasupport.com>
2023-01-18 10:03:57 +0530

    made requests parallel

--- request.js
+++ request.js
@@ -1,11 +1,17 @@
 import fetch from "node-fetch"
 
 (function () {
-    fetch("http://localhost:3010/abc", { 
+    const req1 = fetch("http://localhost:3010/abc", { 
         method: 'POST', 
         body: JSON.stringify({ a: "AAAAA" }), 
         headers: {
            "Content-Type": "application/json"
         }
     })
+
+    const req2 = fetch("http://localhost:3010/abc")
+
+    const allData = Promise.all([req1, req2])
+
+    allData.then((res) => console.log(res));
 })()
